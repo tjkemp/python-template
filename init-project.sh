75c15f800eac3a7f52a7e781c491e3b0d967eee7
tjkemp <tero.kemppi@gmail.com>
2020-11-08 15:27:17 +0200

    Add script to initialize python env

---
+++ init-project.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function process_status () {
+  if [ $? -eq 0 ]; then
+    echo " OK"
+  else
+    echo " FAILED"
+    exit
+  fi
+};
+
+echo Creating a virtual environment...
+python -m venv venv || { process_status; }
+
+echo Updating pip...
+venv/bin/pip install --upgrade pip || { process_status; }
+
+echo Creating requirements.txt...
+pip-compile requirements/requirements.in || { process_status; }
+
+echo Installing dependencies...
+echo pip install -r requirements/requirements.txt || { process_status; }
+
+echo Virtual environment created. Type \"source venv/bin/activate\" to activate it.
